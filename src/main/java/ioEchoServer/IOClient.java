5f8a7e49da7a1f59ae8a315e0806b6f149c351f3
NikitchenkoE <66593735+NikitchenkoE@users.noreply.github.com>
2021-11-03 11:07:44 +0200

    updated code

--- src/main/java/ioEchoServer/IOClient.java
+++ src/main/java/ioEchoServer/IOClient.java
@@ -5,31 +5,22 @@ import java.net.Socket;
 import java.nio.charset.StandardCharsets;
 
 public class IOClient {
-    private static String getMessageFromConsole() throws IOException {
-        InputStream fromConsole = System.in;
-        BufferedInputStream bufferedInputStream = new BufferedInputStream(fromConsole);
-        byte[] bufferFromConsole = new byte[8192];
-        int countFromConsole = bufferedInputStream.read(bufferFromConsole);
-        return new String(bufferFromConsole, 0, countFromConsole);
-    }
-
     public static void main(String[] args) throws IOException {
-        Socket socket = new Socket("localhost", 3000);
-        BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(socket.getOutputStream());
-        BufferedInputStream bufferedInputStream = new BufferedInputStream(socket.getInputStream());
+        byte[] buffer = new byte[8192];
 
-        String stringByConsole = "";
-        while (!stringByConsole.equals("\n")) {
-            stringByConsole = getMessageFromConsole();
-            bufferedOutputStream.write(stringByConsole.getBytes(StandardCharsets.UTF_8));
-            bufferedOutputStream.flush();
+        try (Socket socket = new Socket("localhost", 3000);
+             BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(socket.getOutputStream());
+             BufferedInputStream bufferedInputStream = new BufferedInputStream(socket.getInputStream())) {
 
-            byte[] buffer = new byte[8192];
-            int count = bufferedInputStream.read(buffer);
-            String receivedMessage = new String(buffer, 0, count);
-            System.out.println(receivedMessage);
+            int count;
+            while ((count = System.in.read(buffer)) != 1) {
+                bufferedOutputStream.write(buffer,0,count);
+                bufferedOutputStream.flush();
+
+                int countRecieved = bufferedInputStream.read(buffer);
+                String receivedMessage = new String(buffer, 0, countRecieved);
+                System.out.println(receivedMessage);
+            }
         }
-        bufferedOutputStream.close();
-        bufferedInputStream.close();
     }
 }
--- src/main/java/ioEchoServer/IOServer.java
+++ src/main/java/ioEchoServer/IOServer.java
@@ -6,22 +6,20 @@ import java.net.Socket;
 
 public class IOServer {
     public static void main(String[] args) throws IOException {
-        ServerSocket serverSocket = new ServerSocket(3000);
-        Socket socket = serverSocket.accept();
-        BufferedInputStream bufferedInputStream = new BufferedInputStream(socket.getInputStream());
-        BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(socket.getOutputStream());
-        while (socket.isConnected()) {
-            byte[] buffer = new byte[8192];
-            int count = bufferedInputStream.read(buffer);
-            String receivedMessage = new String(buffer, 0, count);
-            String messageToSendBack = "Echo: " + receivedMessage;
+        try (ServerSocket serverSocket = new ServerSocket(3000);
+             Socket socket = serverSocket.accept();
+             BufferedInputStream bufferedInputStream = new BufferedInputStream(socket.getInputStream());
+             BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(socket.getOutputStream())) {
 
-            bufferedOutputStream.write(messageToSendBack.getBytes());
-            bufferedOutputStream.flush();
+            while (socket.isConnected()) {
+                byte[] buffer = new byte[8192];
+                int count = bufferedInputStream.read(buffer);
+                String receivedMessage = new String(buffer, 0, count);
+                String messageToSendBack = "Echo: " + receivedMessage;
+
+                bufferedOutputStream.write(messageToSendBack.getBytes());
+                bufferedOutputStream.flush();
+            }
         }
-        bufferedInputStream.close();
-        bufferedOutputStream.close();
-        socket.close();
-        serverSocket.close();
     }
 }
